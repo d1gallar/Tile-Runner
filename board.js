80fcbfc3077b86eb5a14a803db82c7fbef85f73c
David Gallardo <gallardo_d@live.com>
2021-11-10 16:15:39 -0800

    added color themes

--- board.js
+++ board.js
@@ -1,28 +1,43 @@
-const MAX_COLORS = 6;
+const ColorTheme = require('./colorTheme'); 
+
 const MAX_TILE = 4;
 const TOTAL_TILES = 25;
 const BOARD_SIZE = 5;
-const DEFAULT_COLORS = ["R", "Y", "B", "G", "O", "W"]
-const EMPTY_COLOR = '';
+const MAX_COLORS = 6;
+const MATCH_SIZE = 3;
 const EMPTY_TILE = ' ';
 
 class Board {
+    
     constructor(){
         this.board = null;
-        this.colors = DEFAULT_COLORS;
+        this.theme = new ColorTheme();
         this.randomFill()
+    }
+
+    //Returns the current board
+    getBoard(){ return this.board;  }
+
+    //Creates a randomly generated 9 x 9 board that the player has to match
+    createMatch(){
+        let colorSet = this.createColorSet();
+        let match = [];
+        for(let i = 0; i < MATCH_SIZE; i++){
+            let row = [];
+            for(let j = 0; j < MATCH_SIZE; j++){
+                let c = Math.floor(Math.random() * colorSet.length);
+                row.push(colorSet[c]);
+                colorSet.splice(c,1);
+            }
+            match.push(row);
+        }
+        return match;
     }
 
     //Fills the board with all the colored tiles
     randomFill(){
-        if(!this.checkColors()) return new Error("Failed to fill the board!");
-        let visited = new Set();
-        let colorSet = new Array();
-        for(let i = 0; i < MAX_COLORS; i++){
-            for(let j = 0; j < MAX_TILE; j++){
-                colorSet.push(this.colors[i]);
-            }
-        }
+        if(!this.theme.isValid()) return new Error("The theme is invalid!");
+        let colorSet = this.createColorSet();
         
         let board = new Array();
         for(let i = 0; i < BOARD_SIZE; i++){
@@ -41,41 +56,16 @@ class Board {
         this.board = board;
     }
 
-    //Checks if all the colors have been selected
-    checkColors(){
-        let missing = this.colors.filter(x => x === EMPTY_COLOR);
-        if(missing.length > 0) {
-            console.log(`WARNING: Missing ${missing.length}/${MAX_COLORS} colors!`)
-            return false;
-        }
-        return true;
-    }
-
-    // Adds a color onto the board if there are any available spots. If all 
-    // colors have been selected, it does not add the color!
-    addColor(color){
+    // Generates all the possibilities of what kind of color a tile could be
+    // Returns a color array that represents the probability 
+    createColorSet(){
+        let colorSet = new Array();
         for(let i = 0; i < MAX_COLORS; i++){
-            if(this.colors[i] === EMPTY_COLOR){
-                this.colors[i] = color;
-                return true;
+            for(let j = 0; j < MAX_TILE; j++){
+                colorSet.push(this.theme.get()[i]);
             }
         }
-        console.log('There are no available spots to add a new color!')
-        return false;
-    }
-    
-    // Sets the color at the specified index. 
-    setColor(color, i){
-        if(i > MAX_COLORS || i < 0) {
-            console.log(`Failed to set ${color} onto the`,
-            `board since ${i} is out of range.`)
-        }
-        this.color[i] = color;
-    }
-
-    // Resets all the board colors
-    resetColors(){
-        this.colors = new Array(MAX_COLORS).fill(EMPTY_COLOR);
+        return colorSet;
     }
 
     // Resets all the board tiles
@@ -87,8 +77,10 @@ class Board {
         }
     }
 
+    // Prints the board out to the console
     print(){ console.log(this.board)}
 }
 
 let board = new Board();
+console.log(board.createMatch());
 board.print();
---
+++ colorTheme.js
@@ -0,0 +1,85 @@
+const DEFAULT_COLORS = ["R", "Y", "B", "G", "O", "W"];
+const MAX_COLORS = 6;
+const EMPTY_COLOR = '';
+
+module.exports = class ColorTheme {
+    constructor(){
+        this.colors = DEFAULT_COLORS;
+    }
+
+    //Returns the current theme
+    get(){ return this.colors };
+
+    //Checks if there are 6 colors in the theme
+    isValid(){
+        let missing = this.colors.filter(x => x === EMPTY_COLOR);
+        if(missing.length > 0) {
+            console.log(
+                new Error(`The theme is invalid. Missing`,
+                `${missing.length}/${MAX_COLORS} colors!`))
+            return false;
+        }
+        return true;
+    }
+
+    // Adds a color onto the theme if there are any available spots. 
+    // It inserts the color at the next available spot. If there are no
+    // available spots, it does not add the color into the current theme.
+    add(color){
+        for(let i = 0; i < MAX_COLORS; i++){
+            if(this.colors[i] === EMPTY_COLOR){
+                this.colors[i] = color;
+                return true;
+            }
+        }
+        console.log(
+            new Error('There are no available spots to add a new color!'))
+        return false;
+    }
+
+    // Changes the color at the specified index. 
+    setColorAt(color, i){
+        if(i > MAX_COLORS || i < 0) {
+            console.log(`Failed to set ${color} onto the`,
+            `board since ${i} is out of range.`)
+        }
+        this.color[i] = color;
+    }
+
+    // Takes in a theme (an array of 6 colors) and sets it as the new theme
+    // XXXXX Work on checking if each element is a "color"
+    set(colors){
+        if(colors === null){
+            return console.log(new Error(`The theme is empty or null!`));
+            
+        } 
+        if(!(colors instanceof Array)){
+            return console.log(
+                new Error(`The theme is not an array of colors!`));
+        }
+        if(colors.length !== MAX_COLORS){
+            if(colors.length === MAX_COLORS-1){
+                return console.log(
+                    new Error(`Missing ${MAX_COLORS-colors.length} color!`));
+            } else {
+                return console.log(
+                    new Error(`Missing ${MAX_COLORS-colors.length} colors!`));
+            } 
+        }
+        for(let i = 0; i < MAX_COLORS; i++){
+            this.colors[i] = colors[i];
+        }
+    }
+
+    // Resets the color theme by making it empty
+    reset(){
+        this.colors = new Array(MAX_COLORS).fill(EMPTY_COLOR);
+    }
+
+    // Resets the theme to the default color theme
+    resetDefault(){
+        this.colors = DEFAULT_COLORS;
+    }
+
+    print(){ console.log(this.colors)}
+}
